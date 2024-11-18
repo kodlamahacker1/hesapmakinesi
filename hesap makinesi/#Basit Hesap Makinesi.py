de21f9beb5431d6eec7a26cd3f0435e18378a5da
kodlamahacker1 <kodlamahacker1@gmail.com>
2024-11-18 18:57:06 +0300

    Add files via upload

---
+++ hesap makinesi/#Basit Hesap Makinesi.py
@@ -0,0 +1,16 @@
+#Basit Hesap Makinesi
+
+print("Basit Hesap Makinesine Hoş Geldiniz")
+
+a = float(input("ilk sayı: "))
+b = float(input("ikinci sayı: "))
+
+toplama = a + b
+çıkarma = a - b
+çarpma = a * b
+bölme = a / b
+
+print("Toplama sonucu: ", toplama)
+print("Çıkarma sonucu: ", çıkarma)
+print("Çarpma sonucu: ", çarpma)
+print("Bölme sonucu: ", bölme)
